2e8c2f942241d8dae70e9f198cf34b0a18f13812
rafaax <rafavfx1@gmail.com>
2024-01-03 11:05:56 -0300

    asdsad

--- kanban.php
+++ kanban.php
@@ -175,5 +175,4 @@ if(isset($_GET['cadastro'])){
 <script src="vendor/animsition/animsition.min.js"></script>
 <script src="js/main.js"></script>
 <script src="assets/js/jkanban.js"></script>
-
 <script src="js/kanban.js"></script>
